af92383d255388261b408b93804b13dc1acef16f
FakeSayt <141634440+FakeSayt@users.noreply.github.com>
2026-01-27 02:29:23 +0100

    Add files via upload

---
+++ artifacts.py
@@ -0,0 +1,7 @@
+# Lista przykładowych artefaktów
+ARTIFACTS = {
+    "Rapid": "Rapid increases attack speed by 20%",
+    "Iron Fist": "Iron Fist gives 2x Physical Attack bonus and Crit",
+    "Surge": "Surge increases dodge % and defense stats",
+    # możesz dodać więcej artefaktów
+}
